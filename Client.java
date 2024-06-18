24ba62402062e7bea6840fab62c522ea0ba6ec9f
Sanjeevani <sanjeevanisharma741390pal@gmail.com>
2024-06-18 23:39:27 +0530

    Socket()

--- Client.java
+++ Client.java
@@ -1,32 +1,106 @@
 import java.net.Socket;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.*;
 
-public class Client 
+public class Client extends JFrame
 {
 
     Socket socket;
     BufferedReader br;
     PrintWriter out;
+    //delete components
+    private JLabel heading=new JLabel("Client Area");
+    private JTextArea messageArea= new JTextArea();
+    private JTextField messageInput= new JTextField();
+    private Font font = new Font("Roboto",Font.PLAIN,20);
 
+    //constructor
     public Client()
     {
         try {
-            System.out.println("Sending request to a server");
-            socket=new Socket("127.0.0.1",7777);
-            System.out.println("Connection done..");
+            //System.out.println("Sending request to a server");
+            //socket=new Socket("127.0.0.1",7777);
+            //System.out.println("Connection done..");
 
-            br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            //br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            out=new PrintWriter(socket.getOutputStream());
-
-            startReading();
-            startWriting();
+            //out=new PrintWriter(socket.getOutputStream());
+            createGUI();
+            handleEvents();
+            //startReading();
+            //startWriting();
         } catch (Exception e) {
             // TODO: handle exception
         }
     } 
 
+    private void handleEvents()
+    {
+        messageInput.addKeyListener(new KeyListener() {
 
+            @Override
+            public void keyTyped(KeyEvent e) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                // TODO Auto-generated method stub
+                System.out.println("Key Released "+e.getKeyCode());
+            }
+            
+        });
+    }
+
+    private void createGUI()
+    {
+
+        //gui code
+
+        this.setTitle("Client Messager[END]");
+        this.setSize(600, 700);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //coding for component
+        heading.setFont(font);
+        messageArea.setFont(font);
+        messageInput.setFont(font);
+        heading.setIcon(new ImageIcon("chat+.jpg"));
+        heading.setHorizontalTextPosition(SwingConstants.CENTER);
+        heading.setVerticalTextPosition(SwingConstants.BOTTOM);
+        heading.setHorizontalAlignment(SwingConstants.CENTER);
+        heading.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+        messageInput.setHorizontalAlignment(SwingConstants.CENTER);
+        //frames layout
+        this.setLayout(new BorderLayout());
+        
+        //adding the component to frame
+        this.add(heading,BorderLayout.NORTH);
+        this.add(messageArea, BorderLayout.CENTER);
+        this.add(messageInput, BorderLayout.SOUTH);
+        this.setVisible(true);
+    }
+
+    //start reading method
     public void startReading()
     {
         Runnable r1 = () -> {
@@ -59,6 +133,7 @@ public class Client
         new Thread(r1).start();
     }
 
+    //start writing
     public void startWriting() 
     {
         Runnable r2 = () -> {
